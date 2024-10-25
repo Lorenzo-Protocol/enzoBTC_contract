62a9573b4e5bade5470c921c4c74b0fa362f7a69
star03x <wednesday117@proton.me>
2024-10-25 14:28:13 +0800

    __gap size 50

--- src/modules/WithdrawalRequest.sol
+++ src/modules/WithdrawalRequest.sol
@@ -186,5 +186,5 @@ abstract contract WithdrawalRequest is Initializable, BlackList, IWithdrawalRequ
      * variables without shifting down storage in the inheritance chain.
      * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
      */
-    uint256[49] private __gap;
+    uint256[50] private __gap;
 }
